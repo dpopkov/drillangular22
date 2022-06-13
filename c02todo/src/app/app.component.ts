4c6d90e40ab8d78b41f08b1983f1c4a4d0194d08
Denis Popkov <pkvdenis@gmail.com>
2022-06-13 16:45:16 +0300

    c02todo: add model classes for TodoItem and TodoList, display initial info in app.component.

--- c02todo/src/app/app.component.ts
+++ c02todo/src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {TodoList} from "./model/todoList";
+import {TodoItem} from "./model/todoItem";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'c02todo';
+  private list = new TodoList("Alice", [
+    new TodoItem("Go for long run", true),
+    new TodoItem("Get some milk"),
+    new TodoItem("Collect garbage")
+  ]);
+
+  get username(): string {
+    return this.list.user;
+  }
+
+  get itemCount(): number {
+    return this.list.items.filter(item => !item.complete).length;
+  }
 }
